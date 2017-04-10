d97b476f81275023a79a3c774d1a7d7a463a14ca
dmitriy-serdyuk <serdyuk.dmitriy@gmail.com>
2017-04-10 16:08:48 -0400

    Add pull scripts

---
+++ pull.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+KALDI_MASTER="https://github.com/kaldi-asr/kaldi"
+SOURCE_BRANCH="master"
+TARGET_BRANCH="master"
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping pull; just doing a build."
+    exit 0
+fi
+
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+# Clone the existing gh-pages for this repo into out/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+git clone ${KALDI_MASTER} kaldi-master
+pushd kaldi-master
+#git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+# Now that we're all set up, we can push.
+gti remote add dest $REPO
+git push dest $TARGET_BRANCH
+popd
+
